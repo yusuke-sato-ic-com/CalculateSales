148887b8ece4b2a2b350b25f1488e999dab52f5c
佐藤悠介 <yusuke_sato@ic-com.co.jp>
2016-03-16 11:23:52 +0900

    追記
    集計別ファイルの出力追記
    コメント文などを整理

--- src/jp/co/iccom/sato_yusuke/calculate_sales/CalculateSales.java
+++ src/jp/co/iccom/sato_yusuke/calculate_sales/CalculateSales.java
@@ -6,23 +6,23 @@ import java.util.Map.Entry;
 
 public class CalculateSales {
 	public static void main(String[] args) {
-		// コマンドライン引数で、ディレクトリより支店定義ファイルを読み込む
+		// コマンドライン引数で指定したディレクトリより、定義ファイルを読み込む
 		// ディレクトリまでのパス指定
 		File branch = new File(args[0],"branch.lst"); // Fileオブジェクトの生成
 		File commodity = new File(args[0],"commodity.lst");
 		HashMap<String, String> branchMap = new HashMap<String, String>();
 		HashMap<String, String> commodityMap = new HashMap<String, String>();
 
+		// 支店定義ファイルの読み込み
 		try {
-			FileReader fr1 = new FileReader(branch);
-			BufferedReader br1 = new BufferedReader(fr1);
-			String line1;
-			while ((line1 = br1.readLine()) != null) {
-				String[] branchData = line1.split(",");
-			//	System.out.println(branchData[0] + branchData[1]);
+			FileReader fr = new FileReader(branch);
+			BufferedReader br = new BufferedReader(fr);
+			String line;
+			while ((line = br.readLine()) != null) {
+				String[] branchData = line.split(",");
 				branchMap.put(branchData[0], branchData[1]);
 			}
-			br1.close(); //ストリームを閉じる
+			br.close(); //ストリームを閉じる
 		} catch(FileNotFoundException fe)  { // 例外を受け取る
 				System.out.println("支店定義ファイルは存在しません");
 				return;
@@ -30,17 +30,17 @@ public class CalculateSales {
 			System.out.println("予期せぬエラーが発生しました");
 		}
 
+		// 商品定義ファイルの読み込み
 		try {
-			FileReader fr2 = new FileReader(commodity);
-			BufferedReader br2 = new BufferedReader(fr2);
-			String line2;
-			while ((line2 = br2.readLine()) != null) {
-				String[] commodityData = line2.split(",");
-			//	System.out.println(commodityData[0] + commodityData[1]);
+			FileReader fr = new FileReader(commodity);
+			BufferedReader br = new BufferedReader(fr);
+			String line;
+			while ((line = br.readLine()) != null) {
+				String[] commodityData = line.split(",");
 //			//	ここで集計用Mapの初期値0を格納した方がすっきりする
 				commodityMap.put(commodityData[0], commodityData[1]);
 			}
-			br2.close();
+			br.close();
 		} catch(FileNotFoundException fe)  { // 例外を受け取る
 			System.out.println("商品定義ファイルは存在しません");
 			return;
@@ -48,7 +48,7 @@ public class CalculateSales {
 			System.out.println("予期せぬエラーが発生しました");
 		}
 
-		// ディレクトリ内のファイル一覧を取得
+		// 指定ディレクトリ内のファイル一覧を取得
 		String path = args[0];
 		File dir = new File(path);
 		File[] files = dir.listFiles(); // dir内のファイルを配列に格納
@@ -67,7 +67,7 @@ public class CalculateSales {
 			}
 		}
 
-		// 連番チェック
+		// 連番エラーのチェック
 		for (int i = 0; i < salesNo.size(); i++) {
 			if(salesNo.get(i) != i + 1){
 				System.out.println("売上ファイル名が連番になっていません");
@@ -75,8 +75,8 @@ public class CalculateSales {
 			}
 		}
 
-		// 支店コード・商品コードをkey、金額をvalueに格納する集計用Mapを作成
 //		// 定義ファイルの格納時点にまとめた方がすっきりする
+		// 支店コード・商品コードをkey、金額をvalueに格納する集計用Mapを作成
 		HashMap<String, Integer> rcdBranchSalesMap = new HashMap<String, Integer>();
 		HashMap<String, Integer> rcdCommoditySalesMap = new HashMap<String, Integer>();
 
@@ -104,16 +104,16 @@ public class CalculateSales {
 				br.close();
 
 			} catch (IOException e ) {
-				System.out.println(e);
+				System.out.println("予期せぬエラーが発生しました");
 			}
 
 			// 該当コード毎に売上金額を格納、加算
 			String branchCodes = rcdDataList.get(0);
 			String commodityCodes = rcdDataList.get(1);
 			int amount = Integer.parseInt(rcdDataList.get(2));
-// int型は桁数の問題あり
+//			int型は桁数の問題あり
 
-//			// 1つにまとめる！
+//			// 1つにまとめた方がいい？
 			if (rcdBranchSalesMap.containsKey(branchCodes)) {
 				int branchAmount = rcdBranchSalesMap.get(branchCodes) + amount;
 				rcdBranchSalesMap.put(branchCodes, branchAmount);
@@ -125,14 +125,7 @@ public class CalculateSales {
 			}
 		}
 
-	//	System.out.println(rcdBranchSalesMap);
-	//	System.out.println(rcdCommoditySalesMap);
-
-	//	System.out.println(branchMap.entrySet());
-	//	System.out.println(commodityMap.entrySet());
-
-
-
+		// 支店別
 		// 合計金額の降順に並び替える
 		// ソート用List生成
 		List<Map.Entry<String,Integer>> branchSales =
@@ -146,32 +139,64 @@ public class CalculateSales {
 			}
 		});
 
-		// 集計結果を出力する為に
-		// 支店別集計ファイルの作成
+		// 集計結果を出力する為、支店別集計ファイルの作成
 		File newBranchOut = new File(args[0],"branch.out");
 		try{
-			if (newBranchOut.createNewFile()){
-				System.out.println("さて、ファイルの中身は何かな？");
-			}else{
-				System.out.println("ファイルの作成に失敗しました");
-			}
+			newBranchOut.createNewFile();
 		} catch(IOException e) {
-			System.out.println(e);
+			System.out.println("予期せぬエラーが発生しました");
 		}
 
 		// 支店別ファイルに出力
-		for (Entry<String,Integer> s : branchSales) { // keyとvalueを拡張for文で取得
-			if (branchMap.containsKey(s.getKey())) {
-		//		System.out.println(s.getKey() + "," + branchMap.get(s.getKey()) + "," + s.getValue());
+		try {
+			FileWriter fw = new FileWriter(newBranchOut);
+			BufferedWriter bw = new BufferedWriter(fw);
+			for (Entry<String,Integer> s : branchSales) { // keyとvalueを拡張for文で取得。拡張for文の位置に注意！
+				if (branchMap.containsKey(s.getKey())) {
+					bw.write(s.getKey() + "," + branchMap.get(s.getKey()) + "," + s.getValue());
+					bw.newLine();
+				}
 			}
-			try {
-				FileWriter fw = new FileWriter(newBranchOut);
-				BufferedWriter bw = new BufferedWriter(fw);
-				bw.write(s.getKey() + "," + branchMap.get(s.getKey()) + "," + s.getValue());
-				bw.close(); //ストリームを閉じる
-			} catch(IOException e)  { // 例外を受け取る
-				System.out.println(e);
+			bw.close();
+			} catch(IOException e)  {
+				System.out.println("予期せぬエラーが発生しました");
+		}
+
+		// 商品別
+		// 合計金額の降順に並び替える
+		// ソート用List生成
+		List<Map.Entry<String,Integer>> commoditySales =
+				new ArrayList<Map.Entry<String,Integer>>(rcdCommoditySalesMap.entrySet());
+		Collections.sort(commoditySales, new Comparator<Map.Entry<String,Integer>>() {
+
+			@Override
+			public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+			// TODO 自動生成されたメソッド・スタブ
+			return  ((Integer)o2.getValue()).compareTo((Integer)o1.getValue());
 			}
+		});
+
+		// 集計結果を出力する為、商品別集計ファイルの作成
+		File newCommodityOut = new File(args[0],"commodity.out");
+		try{
+			newCommodityOut.createNewFile();
+		} catch(IOException e) {
+				System.out.println("予期せぬエラーが発生しました");
+		}
+
+		// 商品別ファイルに出力
+		try {
+			FileWriter fw = new FileWriter(newCommodityOut);
+			BufferedWriter bw = new BufferedWriter(fw);
+			for (Entry<String,Integer> s : commoditySales) { // keyとvalueを拡張for文で取得。拡張for文の位置に注意！
+				if (commodityMap.containsKey(s.getKey())) {
+					bw.write(s.getKey() + "," + commodityMap.get(s.getKey()) + "," + s.getValue());
+					bw.newLine();
+				}
+			}
+			bw.close();
+			} catch(IOException e)  {
+				System.out.println("予期せぬエラーが発生しました");
 		}
 	}
 }
